fefdf4342f12d35e033a29b0228905231131721d
Uzondu Okechukwu <uzonduokey30@gmail.com>
2025-10-22 03:18:59 +0100

    home set up

--- src/app/Display/[id]/page.tsx
+++ src/app/Display/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { prisma } from "@/app/utils/db"
+import Link from "next/link"
 
 interface props {
     params: { id: string }
@@ -24,7 +25,7 @@ export default async function Content({ params }: props) {
 
     return (
         <div className="max-w-100 mx-auto">
-
+            <Link href='/' className="hover:underline">back to home</Link>
 
             <h1 className="text-3xl font-bold text-green-600 mb-4">{data.title}</h1>
             <p className="bg-gray-100 p-4 rounded">{data.content}</p>
