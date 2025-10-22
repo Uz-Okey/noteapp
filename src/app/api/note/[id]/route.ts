cf6554d1754690be8b6b76ec6b8e543c882c3310
Uzondu Okechukwu <uzonduokey30@gmail.com>
2025-10-22 01:49:09 +0100

    note book vercel set up

--- src/app/api/note/[id]/route.ts
+++ src/app/api/note/[id]/route.ts
@@ -1,5 +1,6 @@
 import { prisma } from "@/app/utils/db";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+
 
 export async function DELETE(
   request: Request,
@@ -22,33 +23,24 @@ export async function DELETE(
 
 
 export async function PATCH(
-  request: Request,
-  { params }: { params: { id: string } }
+  request: NextRequest,
+  context: { params: Promise<{ id: string }> }
 ) {
-  try {
-    const body = await request.json(); // get new title and content
-    const { title, content } = body;
+  const { id } = await context.params;
+  const body = await request.json();
+  const { title, content } = body;
 
-    // ✅ check if title or content is missing
-    if (!title || !content) {
-      return NextResponse.json(
-        { message: "Title and content are required" },
-        { status: 400 }
-      );
-    }
-
-    // ✅ update the note
-    const updatedNote = await prisma.note.update({
-      where: { id: params.id },
-      data: { title, content },
-    });
-
-    return NextResponse.json(updatedNote, { status: 200 });
-  } catch (err) {
-    console.error("Error updating note:", err);
+  if (!title || !content) {
     return NextResponse.json(
-      { message: "Failed to update note" },
-      { status: 500 }
+      { message: "Title and content required" },
+      { status: 400 }
     );
   }
+
+  const newUpdate = await prisma.note.update({
+    where: { id },
+    data: { title, content },
+  });
+
+  return NextResponse.json(newUpdate, { status: 200 });
 }
